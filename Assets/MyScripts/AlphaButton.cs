625aae4b98f4bb226bd036aec07fcfb62085a5f7
SirDolittle <noahjb1999@gmail.com>
2019-10-16 10:27:37 +0100

    Workng Gravity Shift UI
    I have installed a working UI for shifting the gravity of the scene. While holding shift the UI appears and time slows down. It still needs work as the player still rotates while usng the UI and the the backgorund isn't blured.

---
+++ Assets/MyScripts/AlphaButton.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI; // Required when Using UI elements.
+
+public class AlphaButton : MonoBehaviour
+{
+    public float AlphaThreshold = 0.1f;
+
+    void Start()
+    {
+        this.GetComponent<Image>().alphaHitTestMinimumThreshold = AlphaThreshold;
+    }
+}
---
+++ Assets/MyScripts/GravityUIController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI; // Required when Using UI elements.
+
+
+public class GravityUIController : MonoBehaviour
+{
+
+    public Button B_Forward, B_Right, B_Left, B_Backwards, B_UP;
+    public bool[] buttonUsed;
+    private GameObject player;
+    private void Start()
+    {
+      player = GameObject.FindWithTag("Player");
+
+        B_Forward.onClick.AddListener(player.GetComponent<CharacterController>().GravityForwards);
+        B_Right.onClick.AddListener(player.GetComponent<CharacterController>().GravityRight);
+        B_Left.onClick.AddListener(player.GetComponent<CharacterController>().GravityLeft);
+        B_Backwards.onClick.AddListener(player.GetComponent<CharacterController>().GravityBackwards);
+        B_UP.onClick.AddListener(player.GetComponent<CharacterController>().GravityUp);
+        buttonUsed = new bool[5];
+    }
+
+}
