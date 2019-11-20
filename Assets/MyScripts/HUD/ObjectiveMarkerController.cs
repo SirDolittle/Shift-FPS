dc54360086365b556ccd1747f47830c96cdf90d2
SirDolittle <noahjb1999@gmail.com>
2019-11-20 20:48:38 +0000

    Added the gravity engineer speech box

--- Assets/MyScripts/HUD/ObjectiveMarkerController.cs
+++ Assets/MyScripts/HUD/ObjectiveMarkerController.cs
@@ -4,12 +4,17 @@ using UnityEngine;
 
 public class ObjectiveMarkerController : MonoBehaviour
 {
-    ObjectiveBools objectiveBools;
+    public GameObject[] ObjectiveMarkerLocations;
+
     // Start is called before the first frame update
     void Start()
     {
         
     }
 
-   
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
 }
---
+++ Assets/MyScripts/HUD/Speech/UITypeEffect.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI; 
+public class UITypeEffect : MonoBehaviour
+{
+    Text txt;
+    string story;
+
+
+    public void StartTyping()
+    {
+        txt = GetComponent<Text>();
+        story = txt.text;
+        txt.text = "";
+
+        // TODO: add optional delay when to start
+        StartCoroutine("PlayText");
+    }
+
+    IEnumerator PlayText()
+    {
+        foreach (char c in story)
+        {
+            txt.text += c;
+            yield return new WaitForSeconds(0.125f);
+        }
+    }
+}
